9c612cdb9709aff28a79a59074a8621b547c552a
Pedro Freitas <soufreitas.pedro@gmail.com>
2018-06-15 20:17:10 -0300

    Adicionando importações, adicionando imagens.

--- js/flappy.js
+++ js/flappy.js
@@ -1,7 +1,21 @@
-var tela = document.querySelector('canvas');
+var tela = document.querySelector('#canvas');
 var pincel = tela.getContext('2d');
 
-pincel.fillStyle = '#33CCFF';
-pincel.fillRect(0,0,tela.width,tela.height);
-pincel.fillStroke = 'black';
-pincel.strokeRect(0,0,tela.width,tela.height);
+var imagens = new ImageLibrary();
+imagens.load("fish", "imagens/RedFish2.png");
+imagens.load("backg", "imagens/sky.png");
+imagens.load("enter", "imagens/enter.png");
+imagens.load("rank", "imagens/rank.png");
+imagens.load("info", "imagens/info.png");
+imagens.load("voltar", "imagens/voltar.png");
+
+//  --- Sons ---
+//var audios = new AudioLibrary();
+//audios.load("tiro", "sons/tiro.wav");
+
+//  --- Variaveis ---
+var estado = "menu";
+var btn1 = new Botao(400,150,200,70);
+var btn2 = new Botao(400,250,200,70);
+var btn3 = new Botao(400,350,200,70);
+var btn4 = new Botao(135,430,150,20);
