eb48a2b65c5cc118fa2caed9535266b418538007
Pedro Freitas <soufreitas.pedro@gmail.com>
2018-07-16 15:26:20 -0300

    Finalizando

--- flappyBird.js
+++ flappyBird.js
@@ -1,22 +1,22 @@
 var cvs = document.getElementById("canvas");
 var ctx = cvs.getContext("2d");
 
-// load images
+// carregando imagens
 
 var bird = new Image();
 var bg = new Image();
 var fg = new Image();
-var pipeNorth = new Image();
-var pipeSouth = new Image();
+var canoCima = new Image();
+var canoBaixo = new Image();
 
 bird.src = "images/bird.png";
 bg.src = "images/bg.png";
 fg.src = "images/fg.png";
-pipeNorth.src = "images/pipeNorth.png";
-pipeSouth.src = "images/pipeSouth.png";
+canoCima.src = "images/canoCima.png";
+canoBaixo.src = "images/canoBaixo.png";
 
 
-// some variables
+// variáveis
 
 var gap = 85;
 var constant;
@@ -28,7 +28,7 @@ var gravity = 1.5;
 
 var score = 0;
 
-// audio files
+// carregando sons
 
 var fly = new Audio();
 var scor = new Audio();
@@ -36,7 +36,6 @@ var scor = new Audio();
 fly.src = "sounds/fly.mp3";
 scor.src = "sounds/score.mp3";
 
-// on key down
 
 document.addEventListener("keydown",moveUp);
 
@@ -45,11 +44,11 @@ function moveUp(){
     fly.play();
 }
 
-// pipe coordinates
 
-var pipe = [];
 
-pipe[0] = {
+var cano = [];
+
+cano[0] = {
     x : cvs.width,
     y : 0
 };
@@ -57,75 +56,51 @@ pipe[0] = {
 // draw images
 
 function draw(){
-    
+
     ctx.drawImage(bg,0,0);
-    
-    
-    for(var i = 0; i < pipe.length; i++){
-        
-        constant = pipeNorth.height+gap;
-        ctx.drawImage(pipeNorth,pipe[i].x,pipe[i].y);
-        ctx.drawImage(pipeSouth,pipe[i].x,pipe[i].y+constant);
-             
-        pipe[i].x--;
-        
-        if( pipe[i].x == 125 ){
-            pipe.push({
+
+
+    for(var i = 0; i < cano.length; i++){
+
+        constant = canoCima.height+gap;
+        ctx.drawImage(canoCima,cano[i].x,cano[i].y);
+        ctx.drawImage(canoBaixo,cano[i].x,cano[i].y+constant);
+
+        cano[i].x--;
+
+        if( cano[i].x == 125 ){
+            cano.push({
                 x : cvs.width,
-                y : Math.floor(Math.random()*pipeNorth.height)-pipeNorth.height
-            }); 
+                y : Math.floor(Math.random()*canoCima.height)-canoCima.height
+            });
         }
 
         // detect collision
-        
-        if( bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width && (bY <= pipe[i].y + pipeNorth.height || bY+bird.height >= pipe[i].y+constant) || bY + bird.height >=  cvs.height - fg.height){
+
+        if( bX + bird.width >= cano[i].x && bX <= cano[i].x + canoCima.width && (bY <= cano[i].y + canoCima.height || bY+bird.height >= cano[i].y+constant) || bY + bird.height >=  cvs.height - fg.height){
             location.reload(); // reload the page
         }
-        
-        if(pipe[i].x == 5){
+
+        if(cano[i].x == 5){
             score++;
             scor.play();
         }
-        
-        
+
+
     }
 
     ctx.drawImage(fg,0,cvs.height - fg.height);
-    
+
     ctx.drawImage(bird,bX,bY);
-    
+
     bY += gravity;
-    
+
     ctx.fillStyle = "#000";
     ctx.font = "20px Verdana";
     ctx.fillText("Score : "+score,10,cvs.height-20);
-    
+
     requestAnimationFrame(draw);
-    
+
 }
 
 draw();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
